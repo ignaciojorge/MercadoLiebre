1c13a1c39d1d092769d22377c07d93298cbcc28b
Ignacio Jorge <ignaciojorge4@gmail.com>
2023-11-15 16:39:49 -0300

    segundo entregable individual

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require ('express');
 const app = express();
 const path = require ('path');
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo")
-});
+const port = process.env.PORT || 3001
+app.listen(port, ()=>console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html"))
